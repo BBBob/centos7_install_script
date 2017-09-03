eef588e9d674979a265254094493a50eaae111a5
BBBob <xuzhenanqwe@qq.com>
2017-09-03 20:28:47 +0800

    ranger

---
+++ ranger_install.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/ranger/ranger.git
+cd ranger
+sudo easy_install pip
+sudo pip install pylint
+sudo pip install pytest
+sudo pip install flake8
+make
+sudo make install
+
