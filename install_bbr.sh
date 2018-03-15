38fa37a364bb25e5f2b5c18258d8cf364f9a0a6c
BBBob <xuzhenanqwe@qq.com>
2018-03-15 18:59:54 +0800

    update

---
+++ install_bbr.sh
@@ -0,0 +1,3 @@
+wget --no-check-certificate https://raw.githubusercontent.com/kuoruan/shell-scripts/master/ovz-bbr/ovz-bbr-installer.sh
+sudo chmod +x ovz-bbr-installer.sh
+sudo ./ovz-bbr-installer.sh
--- install_tools.sh
+++ install_tools.sh
@@ -8,6 +8,10 @@ wget https://github.com/BBBob/oh-my-zsh/raw/master/tools/install.sh -O - | sh
 # tools start
 sudo yum install wget -y
 sudo yum install htop -y
+sudo yum install aria2.x86_64  -y
+sudo cp aria2start /usr/bin/
+mkdir -p $HOME/.config/aria2
+sudo cp aria2.conf $HOME/.config/aria2/
 # tools end
 
 # ranger start
@@ -32,4 +36,18 @@ sudo ./shadowsocks_install.sh
 sudo yum install virt-what -y
 # virt-what end
 
+# bbr start
+wget --no-check-certificate https://raw.githubusercontent.com/kuoruan/shell-scripts/master/ovz-bbr/ovz-bbr-installer.sh
+sudo chmod +x ovz-bbr-installer.sh
+sudo ./ovz-bbr-installer.sh
+# bbr end
+
+# nginx start
+sudo yum install nginx -y
+# nginx end
+
+# https ssl start
+sudo yum install certbot-nginx -y
+sudo certbot --nginx
+# https ssl  end
 
