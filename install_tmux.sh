529053bec337af3a6aa0fba712bb58d28ab1af7b
BBBob <xuzhenanqwe@qq.com>
2018-08-17 15:43:45 +0800

    update tmux

--- install_tmux.sh
+++ install_tmux.sh
@@ -1,4 +1,23 @@
 #!/bin/sh
+
+# install deps
+sudo yum install gcc kernel-devel ncurses-devel glibc-static make -y
+# install the old version
 sudo yum install tmux -y
+
 git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
 cp config/.tmux.conf $HOME/
+
+# install libevent
+wget https://github.com/downloads/libevent/libevent/libevent-2.0.21-stable.tar.gz
+tar xzvf libevent-2.0.21-stable.tar.gz
+cd libevent-2.0.21-stable
+./configure && make
+sudo make install
+
+# install the last stable version
+wget https://github.com/tmux/tmux/releases/download/2.4/tmux-2.4.tar.gz
+tar xzf tmux-2.4.tar.gz
+cd tmux-2.4
+./configure && make
+sudo make install
