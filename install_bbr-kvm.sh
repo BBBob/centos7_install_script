b5b7277093221982a1ae79b95df8dc26e507ee94
BBBob <xuzhenanqwe@qq.com>
2018-08-17 14:15:01 +0800

    update

---
+++ install_bbr-kvm.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh
+sudo chmod +x bbr.sh
+sudo ./bbr.sh
--- install_bbr-ovz.sh
+++ install_bbr-ovz.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 wget --no-check-certificate https://raw.githubusercontent.com/kuoruan/shell-scripts/master/ovz-bbr/ovz-bbr-installer.sh
 sudo chmod +x ovz-bbr-installer.sh
 sudo ./ovz-bbr-installer.sh
